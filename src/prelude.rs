419862a3d3524d3ec506b8faf9577115bdbe6770
Shalom Yiblet <shalom.yiblet@gmail.com>
2025-10-14 21:39:00 -0700

    feat: add handle & handle_async to prelude

--- src/prelude.rs
+++ src/prelude.rs
@@ -14,6 +14,9 @@ pub use crate::build::{from_fn, once, repeat};
 // Composition
 pub use crate::compose::chain;
 
+// Running coroutines
+pub use crate::handle::{handle, handle_async};
+
 // Transformations
 pub use crate::compose::{map_input, map_return, map_yield};
 
